c465bc61074d4e5738d89fe2500f9abbbff9b299
Dhruv Maheshwari <dhruvmaheshwari1281@gmail.com>
2026-06-04 11:01:07 +0530

    Add recursive method to print numbers from 10 to 1

---
+++ Print 10-1 using recursion.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void printNumber(int n){
+        //base condition 
+        if (n<1){
+            return;
+        }
+        
+        //logic 
+        System.out.print(n+" ");
+        printNumber(n-1);
+    }
+    
+	public static void main(String[] args) {
+		printNumber(10);
+	}
+}
