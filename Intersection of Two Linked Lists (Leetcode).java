b48a588e3f44b41116673e04d5df6c30fcc500c0
Dhruv Maheshwari <dhruvmaheshwari1281@gmail.com>
2026-06-06 15:36:58 +0530

    Create Intersection of Two Linked Lists (Leetcode).java

---
+++ Intersection of Two Linked Lists (Leetcode).java
@@ -0,0 +1,12 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA;
+        ListNode B = headB;
+
+        while(A != B){
+            A = (A == null) ? headB : A.next;
+            B = (B == null) ? headA : B.next;
+        }
+        return A;
+    }
+}
