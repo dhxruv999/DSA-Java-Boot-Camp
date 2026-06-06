84c1000239575aa9050e193fdf966b2f81f44eaf
Dhruv Maheshwari <dhruvmaheshwari1281@gmail.com>
2026-06-06 10:57:52 +0530

    Refactor linked list implementation and methods

--- Linked List.java
+++ Linked List.java
@@ -1,64 +1,70 @@
-class node{
+// Problem - 1
+// deletion of a data from the head of the linkedlist
+class Node {
     int data;
-    node next;
+    Node next;
     
-    node(int data){
-        this.data=data;
-        this.next=null;
+    Node(int data) {
+        this.data = data;
+        this.next = null;
     }
-    
 }
 public class Main
 {
-    static node head = null;
-    public static void traversal(){
-        if(head == null){
-            System.out.println("LinkedList is null"); 
+    static Node head = null;
+    
+    public static void deletefromHead() {
+        if(head == null) {
             return;
-            }
-        node temp = head;
-        temp = head;
-        while(temp!= null){
-            System.out.print(temp.data + " ->-> ");
+        }
+        head = head.next;
+    }
+    
+    public static void deleteFromTail() {
+        if(head == null) {
+            return;
+        }
+        Node temp = head;
+        while(temp.next.next != null){
             temp = temp.next;
         }
-        System.out.print("NULL");
+        temp.next = null;
     }
-    
-    public static void insertAtHead(int data){
-        node newNode = new node(data);
-        newNode.next = head;
-        
-        head = newNode;
-        
-    }
-    
-    public static void insertAtEnd(int data){
-        node newNode = new node(data);
-        node temp = head;
-        
-        while(temp.next != null){
+    public static void deleteFromAnyGivenPosition(int position) {
+        if(head == null){
+            return;
+        }
+        Node temp = head;
+        for(int i = 1; i < position - 1; i++) {
             temp = temp.next;
         }
-        temp.next = newNode;
-        newNode.next = null;
+        temp.next = temp.next.next;
     }
     
-    public static void insertAtAnyGivenPosition(int data, int position){
-        node newNode = new node(data);
-        node temp = head;
-        for (int i = 1; i < position - 1; i++){
+    public static void traversal() {
+        
+        if(head == null){
+            System.out.println("LinkedList is null");
+            return;
+        }
+        
+        Node temp = head;
+        while(temp != null) {
+            System.out.print(temp.data + " --> ");
             temp = temp.next;
         }
-        newNode.next = temp.next;
-        temp.next = newNode;
+        System.out.println("NULL");
     }
-    
 	public static void main(String[] args) {
-		insertAtHead(10);
-		insertAtHead(20);
-		insertAtEnd(30);
-		insertAtAnyGivenPosition(35,3);
-        traversal();
+		head = new Node(10);
+		head.next = new Node(20);
+		head.next.next = new Node(30);
+		head.next.next.next = new Node(40);
+		traversal();
+		deleteFromAnyGivenPosition(3);
+		//deleteFromTail();
+		//deletefromHead();
+		traversal();
+
 	}
 }
