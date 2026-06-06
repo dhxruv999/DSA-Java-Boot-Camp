70a120592c3b4d6adc2fdc5f99cc3b2ffca0f7da
Dhruv Maheshwari <dhruvmaheshwari1281@gmail.com>
2026-06-06 14:58:33 +0530

    Create Palindrome Linked List (Leetcode).java

---
+++ Palindrome Linked List (Leetcode).java
@@ -0,0 +1,41 @@
+class Solution {
+
+    private static ListNode reverseLL(ListNode head) {
+
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+    ListNode secondHalf = reverseLL(slow.next);
+    ListNode first = head;
+    ListNode second = secondHalf;
+    boolean palindrome = true;
+
+    while(second != null) {
+        if(first.val != second.val){
+            palindrome = false;
+            break;
+        }
+        first = first.next;
+        second = second.next;
+    }
+    return palindrome;
+    }
+}
